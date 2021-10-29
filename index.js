6b3583f89342cc1b580caa2251d89844b909dace
Jahangir-Alam-Noman <jahangir2km@gmail>
2021-10-30 00:51:19 +0600

    db connection updated

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.veusr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+console.log(uri);
 
 async function run() {
     try {
         await client.connect();
         const database = client.db("insertDB");
         const haiku = database.collection("haiku");
+        console.log('database connected');
+
 
 
 
@@ -27,7 +30,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
 
 
 app.get('/', (req, res) => {

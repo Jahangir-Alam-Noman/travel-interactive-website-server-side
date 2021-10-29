968d18a5921e274e78b72df624cdaafa201f1d5b
Jahangir-Alam-Noman <jahangir2km@gmail>
2021-10-29 15:10:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.veusr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("insertDB");
+        const haiku = database.collection("haiku");
+
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('V-travel Server is running!');
+})
+
+app.listen(port, () => {
+    console.log('Server running at port', port);
+})
